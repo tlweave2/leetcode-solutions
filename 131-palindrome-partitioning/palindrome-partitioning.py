11d8ab945a0a083494e781942d853052a6175043
Timothy Weaver <tlweave2@asu.edu>
2025-07-13 13:56:36 -0700

    Time: 51 ms (53.73%) | Memory: 34.4 MB (73.05%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,41 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+      
+        result = []
+        current_partition = []
+        
+        def is_palindrome(substring):
+            """Check if string is palindrome using Python slice magic"""
+            return substring == substring[::-1]
+        
+        def backtrack(start_index):
+            """
+            Backtracking function to build all valid partitions
+            start_index: where to start partitioning from
+            """
+            # Base case: reached end of string
+            if start_index == len(s):
+                # Found a complete valid partition!
+                result.append(current_partition[:])  # Make a copy!
+                return
+            
+            # Try all possible cuts from current position
+            for end_index in range(start_index, len(s)):
+                # Extract substring from start_index to end_index (inclusive)
+                substring = s[start_index:end_index + 1]
+                
+                # Check if this substring is a palindrome
+                if is_palindrome(substring):
+                    # CHOOSE: Add to current partition
+                    current_partition.append(substring)
+                    
+                    # EXPLORE: Recursively partition the rest
+                    backtrack(end_index + 1)
+                    
+                    # UNCHOOSE: Remove for backtracking (try other options)
+                    current_partition.pop()
+        
+        # Start backtracking from index 0
+        backtrack(0)
+        return result
+        
