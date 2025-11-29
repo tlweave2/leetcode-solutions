bfc9379029ae0566f1c8d9ef91c1639fc9e78f04
Timothy Weaver <tlweave2@asu.edu>
2025-11-28 22:53:23 -0800

    Time: 31 ms (64.87%) | Memory: 59.4 MB (13.43%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,48 @@
 class Solution {
-       public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);                 // <-- sort enables two-pointer + easy dedupe
-        int n = nums.length;
-
-        for (int i = 0; i < n - 2; i++) {
-            // skip duplicate anchors
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            // since array is sorted, if nums[i] > 0, sum can't be 0 anymore
-            if (nums[i] > 0) break;
-
-            int l = i + 1, r = n - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-
-                    // move both pointers past duplicates
-                    int lv = nums[l], rv = nums[r];
-                    while (l < r && nums[l] == lv) l++;
-                    while (l < r && nums[r] == rv) r--;
-                } else if (sum < 0) {
-                    l++; // need a larger sum
+    public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            // If the smallest element is positive, no solution possible
+            if (nums[i] > 0) {
+                break;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+            
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                
+                if (sum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates for left pointer
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    // Skip duplicates for right pointer
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
                 } else {
-                    r--; // need a smaller sum
+                    right--;
                 }
             }
         }
-        return res;
+        
+        return result;
     }
 }
