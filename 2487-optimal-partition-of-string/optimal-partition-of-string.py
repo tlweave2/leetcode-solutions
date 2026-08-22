47bca55f55f8fbd681422e4a850a18ea548d813d
Timothy Weaver <tlweave2@asu.edu>
2026-08-21 17:28:37 -0700

    Time: 55 ms (49.62%) | Memory: 19.7 MB (79.59%) - LeetSync

---
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        curset = set()
+        res = 1
+        for c in s:
+            if c in curset:
+                res += 1
+                curset = set()
+            curset.add(c)
+        return res
