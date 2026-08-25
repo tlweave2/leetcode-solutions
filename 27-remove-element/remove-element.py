c200d42b6d9ff7776e465c522e41a390aede4f9c
Timothy Weaver <tlweave2@asu.edu>
2026-08-25 12:04:20 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (17.31%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,8 +1,13 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k =0
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k = 0
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]
-                k += 1 
+                k+=1
         return k
