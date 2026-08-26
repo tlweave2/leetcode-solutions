cf333582e7a185b8a560bd9a88d0b2642bec1254
Timothy Weaver <tlweave2@asu.edu>
2026-08-25 21:40:20 -0700

    Time: 16 ms (69.87%) | Memory: 20.2 MB (84.80%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,9 +1,7 @@
 class Solution:
     def canJump(self, nums):
-        reach = 0
-        for i, n in enumerate(nums):
-            if i > reach:
-                return False
-            reach = max(reach, i + n)
-        return True
-        
+        goal = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return goal == 0
